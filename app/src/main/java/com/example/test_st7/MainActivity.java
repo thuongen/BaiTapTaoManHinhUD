aaca73626ce84610991503194825684a826cd9e9
thuongen <enthuong333@gmail.com>
2025-09-27 22:48:01 +0700

    commit 2

--- app/src/main/java/com/example/test_st7/MainActivity.java
+++ app/src/main/java/com/example/test_st7/MainActivity.java
@@ -17,54 +17,32 @@ import com.example.test_st7.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
-
+    // Khai báo biến
+    EditText   adtA,adtB,edtTong;
+    Button btnTong;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Ánh xạ
+        adtA = findViewById(R.id.adtA);
+        adtB = findViewById(R.id.adtB);
+        edtTong = findViewById(R.id.Tong);
+        btnTong = findViewById(R.id.btnTong);
+        // Gán sự kiện
+        btnTong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {// từ adt a => add sang int
+                int a = Integer.parseInt(adtA.getText().toString());
+                int b = Integer.parseInt(adtB.getText().toString());
+                int tong = a + b;
+                edtTong.setText(tong + "");// hiển thị kết quả
+            }
+        });
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+}
 }
